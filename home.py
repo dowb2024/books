96009daba26af7d69fe4129c6e51938b660421b8
dowb2024 <dowb1004@gmail.com>
2024-12-17 19:07:28 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 from datetime import date
-import pymysql
+import time
 import csv
-
 
 # 사이드 바 숨기기
 hide_sidebar_style = """
@@ -14,121 +13,6 @@ hide_sidebar_style = """
     </style>
 """
 st.markdown(hide_sidebar_style, unsafe_allow_html=True)
-
-def get_connection():
-    conn = pymysql.connect(
-        host="127.0.0.1",
-        user="root",
-        password=st.secrets["SQL_PASSWORD"],
-        database="mystore",
-    )
-    return conn
-
-def _execute_insert(conn, sql, buffer):
-    with conn.cursor() as cursor:
-        result = cursor.executemany(sql, buffer)
-        conn.commit()
-        print("write:", result)
-
-
-def bulk_write(conn, sql, data_file_path, batchsize=100):
-    with open(data_file_path, encoding='utf-8') as fr:
-        reader = csv.reader(fr)
-        buffer = []
-        for i, row in enumerate(reader):
-            if i == 0:
-                continue
-            row = [x if x else None for x in row]
-            buffer.append(row)
-            if len(buffer) == batchsize:
-                _execute_insert(conn, sql, buffer)
-                buffer = []
-        if buffer:
-            _execute_insert(conn, sql, buffer)
-
-
-# 데이터 베이스 연결
-conn = get_connection()
-
-# write_books_sql = """
-# INSERT IGNORE INTO
-#     books(
-#         id,
-#         url,
-#         img_url,
-#         title,
-#         authors,
-#         publisher,
-#         published_at,
-#         review_cnt,
-#         rating,
-#         summary,
-#         translation
-#     )
-# VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-# """
-# bulk_write(conn, write_books_sql, "./data/books_metadata.csv", batchsize=100)
-
-def select_list(conn):
-    read_sql = "SELECT * FROM books"
-    with conn.cursor() as cursor:
-        cursor.execute(read_sql)
-        data = cursor.fetchall()
-        df = pd.DataFrame(data, columns=["id", "url", "img_url", "title", "authors", "publisher", "published_at", "review_cnt", "rating", "summary", "translation"])
-    return df
-
-def delete_list(conn, id):
-    delete_sql = f"DELETE FROM books WHERE id = {id}"
-    with conn.cursor() as cursor:
-        cursor.execute(delete_sql)
-        conn.commit()
-
-def select_inout(conn, id):
-    read_sql = f"SELECT * FROM books_inout_history WHERE id={id}"
-    with conn.cursor() as cursor:
-        cursor.execute(read_sql)
-        data = cursor.fetchall()
-        df = pd.DataFrame(data, columns=["id", "register_date", "modify_date", "in_count", "out_count", "description"])
-    return df
-
-def sum_in_count(conn, id):
-    read_sql = f"SELECT sum(in_count) FROM books_inout_history WHERE id={id}"
-    with conn.cursor() as cursor:
-        cursor.execute(read_sql)
-        result = cursor.fetchone()  # 튜플로 한 행을 반환
-
-        if result and result[0] is not None:  # 값이 있을 경우에만 처리
-            data = int(result[0])  # 첫 번째 값만 정수로 변환
-        else:
-            data = 0  # 값이 없거나 NULL일 경우 0으로 설정
-
-    return data
-
-def sum_out_count(conn, id):
-    read_sql = f"SELECT sum(out_count) FROM books_inout_history WHERE id={id}"
-    with conn.cursor() as cursor:
-        cursor.execute(read_sql)
-        result = cursor.fetchone()  # 튜플로 한 행을 반환
-
-        if result and result[0] is not None:  # 값이 있을 경우에만 처리
-            data = int(result[0])  # 첫 번째 값만 정수로 변환
-        else:
-            data = 0  # 값이 없거나 NULL일 경우 0으로 설정
-
-    return data
-
-# @st.cache_data
-# def load_data():
-#     df = pd.read_csv("./data/books_metadata1.csv")
-#     return df
-#
-# # 데이터 샘플
-# df = load_data()
-
-# 변수 설정
-items_per_page = 10
-url = "http://localhost:8501"
-
 
 # 현재 페이지 상태 관리
 if "type" not in st.session_state and "page" not in st.session_state:
@@ -143,13 +27,53 @@ current_type = st.query_params.to_dict()["type"]
 current_search_class = st.query_params.to_dict()["class"]
 current_search_text = st.query_params.to_dict()["text"]
 current_page = st.query_params.to_dict()["page"]
+
 st.session_state.type = current_type
 st.session_state.search_class = current_search_class
 st.session_state.search_text = current_search_text
 st.session_state.page = int(current_page)
 
 
-def view_list(conn, df, items_per_page):
+def load_data():
+    df = pd.read_csv("./data/books_metadata.csv")
+    return df
+
+def load_inout_data():
+    df_inout = pd.read_csv("./data/books_inout_history.csv")
+    return df_inout
+
+def select_inout(df_inout, id):
+    data = []
+    for i in range(len(df_inout)):
+        if df_inout.iloc[i][0] == id:
+            data.append(df_inout.iloc[i])
+    df = pd.DataFrame(data, columns=["id", "register_date", "modify_date", "in_count", "out_count", "description"])
+    return df
+
+
+def sum_in_count(df_inout, id):
+    data = 0
+    for i in range(len(df_inout)):
+        if df_inout.iloc[i][0] == id:
+            data += df_inout.iloc[i][3]
+    return data
+
+def sum_out_count(df_inout, id):
+    data = 0
+    for i in range(len(df_inout)):
+        if df_inout.iloc[i][0] == id:
+            data += df_inout.iloc[i][4]
+    return data
+
+def delete_list(df, id):
+    data = []
+    for i in range(len(df)):
+        if df.iloc[i][0] != id:
+            data.append(df.iloc[i])
+    result = pd.DataFrame(data, columns=["id", "url", "img_url", "title", "authors", "publisher", "published_at", "review_cnt", "rating", "summary", "translation"])
+    result.to_csv('./data/books_metadata.csv', index=False)
+
+def view_list(df, df_inout, items_per_page):
 
     # 총 페이지 수 계산
     total_pages = len(df) // items_per_page + (1 if len(df) % items_per_page != 0 else 0)
@@ -189,10 +113,10 @@ def view_list(conn, df, items_per_page):
         with col4:
             st.markdown(current_data.iloc[i][4])
         with col5:
-            if len(select_inout(conn, current_data.iloc[i][0])) != 0:
+            if len(select_inout(df_inout, current_data.iloc[i][0])) != 0:
                 st.markdown(f'''
                     <a href="{url}/inout?type={st.session_state.type}&class={st.session_state.search_class}&text={st.session_state.search_text}&page={st.session_state.page}&id={current_data.iloc[i][0]}" target="_parent">
-                        {sum_in_count(conn, current_data.iloc[i][0]) - sum_out_count(conn, current_data.iloc[i][0])}
+                        {sum_in_count(df_inout, current_data.iloc[i][0]) - sum_out_count(df_inout, current_data.iloc[i][0])}
                     </a>
                 ''', unsafe_allow_html=True)
             else:
@@ -203,7 +127,9 @@ def view_list(conn, df, items_per_page):
                ''', unsafe_allow_html=True)
         with col6:
             if st.button("삭제", key=f"{current_data.iloc[i][0]}"):
-                delete_list(conn, current_data.iloc[i][0])
+                delete_list(df, current_data.iloc[i][0])
+                time.sleep(3)  # 3초 대기
+                st.rerun()
 
     # 페이지 번호 표시 (링크 형태로)
     st.markdown("")
@@ -282,17 +208,20 @@ def search_books(books):
     return df_result
 
 
-
 # 오늘의 날짜 가져오기
 today = date.today()
 
+# 변수 설정
+items_per_page = 10
+url = "http://localhost:8501"
+
 # 화면에 오늘 날짜 표시
 st.title("ADMIN OF RGT BOOKS STORE")
-st.markdown(f"{today.strftime('%Y.%m.%d')}, made by 이헤진")
+st.markdown(f"{today.strftime('%Y.%m.%d')}, made by 이혜진")
 
 # 등록 링크
 st.write("")
-col1, col2 = st.columns(2)
+col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.markdown(f'''
             <a href="{url}/?type=all&class=&text=&page=1" target="_parent">
@@ -307,6 +236,10 @@ with col2:
             </a>
         ''', unsafe_allow_html=True)
     st.write("")
+with col3:
+    st.markdown("")
+with col4:
+    st.markdown("")
 
 # 검색 입력창
 with st.form("form"):
@@ -328,18 +261,34 @@ if submitted:
         st.session_state.search_class = search_class
         st.session_state.search_text = search_text
         st.session_state.page = 1
-        df = select_list(conn)
-        result = search_books(df)
-        view_list(conn, result, items_per_page)
+
+        # 데이터 샘플
+        df = load_data()
+        df_inout = load_inout_data()
+        if len(df) > 0:
+            result = search_books(df)
+            if len(result) > 0:
+                view_list(result, df_inout, items_per_page)
+            else:
+                st.markdown("검색결과가 없습니다!")
+
 
 elif st.session_state.type == "search":
-    df = select_list(conn)
-    result = search_books(df)
-    view_list(conn, result, items_per_page)
-
+    # 데이터 샘플
+    df = load_data()
+    df_inout = load_inout_data()
+    if len(df) > 0:
+        result = search_books(df)
+        if len(result) > 0:
+            view_list(result, df_inout, items_per_page)
+        else:
+            st.markdown("검색결과가 없습니다!")
 else:
-    df = select_list(conn)
-    view_list(conn, df, items_per_page)
+    # 데이터 샘플
+    df = load_data()
+    df_inout = load_inout_data()
+    if len(df) > 0:
+        view_list(df, df_inout, items_per_page)
 
 
 
